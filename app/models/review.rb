1b55045e76423242e11c430d241ce3f34476ce27
Raghu Betina <raghu@firstdraft.com>
2016-10-02 17:09:13 +0000

    Direct association between reviews and review_replies

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,9 @@
 class Review < ApplicationRecord
   # Direct associations
 
+  has_one    :review_reply,
+             :dependent => :destroy
+
   has_many   :photo_reviews,
              :dependent => :destroy
 
