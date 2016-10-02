b55cdfbe464044102733edb51a32541312356cbf
Raghu Betina <raghu@firstdraft.com>
2016-10-02 17:09:13 +0000

    Direct association between restaurant_owners and review_replies

--- app/models/restaurant_owner.rb
+++ app/models/restaurant_owner.rb
@@ -1,6 +1,10 @@
 class RestaurantOwner < ApplicationRecord
   # Direct associations
 
+  has_many   :review_replies,
+             :foreign_key => "owner_id",
+             :dependent => :destroy
+
   has_many   :restaurant_ownerships,
              :dependent => :destroy
 
--- app/models/review_reply.rb
+++ app/models/review_reply.rb
@@ -1,6 +1,9 @@
 class ReviewReply < ApplicationRecord
   # Direct associations
 
+  belongs_to :owner,
+             :class_name => "RestaurantOwner"
+
   belongs_to :review
 
   # Indirect associations
